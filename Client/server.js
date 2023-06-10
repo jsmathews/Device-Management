6703b89535d18a8958fd07fb9ee267aa3494a240
DESKTOP-2EASLFB\mathe <jsmathews96@gmail.com>
2023-06-10 15:58:05 +0200

    gitignore and env files updated

--- Client/server.js
+++ Client/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+require('dotenv').config()
 
 app.use(cors())
 app.use(express.json());
@@ -9,9 +10,13 @@ app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    // res.send('hello from container')
 });
 
 const PORT = process.env.PORT || 3000;
-console.log('server started on port:', PORT);
-app.listen(PORT);
+app.listen(PORT, (error) => {
+    if (error) {
+        console.error('An error occurred while starting the server:', error);
+    } else {
+        console.log('Client listening on port:', PORT);
+    }
+});
--- Server/server.js
+++ Server/server.js
@@ -4,10 +4,13 @@ const path = require('path');
 const cors = require('cors');
 const mysql = require('mysql');
 const waitOn = require('wait-on');
+require('dotenv').config()
+
+var db;
+var mySqlHost = process.env.STATUS == 'docker' ? (process.env.DOCKER_MYSQL_HOST) : (process.env.DEV_MYSQL_HOST);
 
 const mysqlOptions = {
-    // resources: ['tcp:db:3306'], // Replace with the appropriate host and port of your MySQL service
-    resources: ['tcp:db:3306'], // Replace with the appropriate host and port of your MySQL service
+    resources: [`tcp:${mySqlHost}:3306`], // Replace with the appropriate host and port of your MySQL service
     timeout: 60000, // Maximum time to wait in milliseconds (adjust as needed)
 };
 
@@ -18,16 +21,22 @@ waitOn(mysqlOptions)
         connectToMySQL();
 
         const PORT = process.env.PORT || 5000;
-        console.log('server started on port:', PORT);
-        app.listen(PORT);
+        app.listen(PORT, (error) => {
+            if (error) {
+                console.error('An error occurred while starting the server:', error);
+            } else {
+                console.log('server listening on port:', PORT);
+            }
+        });
+
     })
     .catch((error) => {
         console.error('Error waiting for MySQL service:', error);
     });
-var db
+
 function connectToMySQL() {
     db = mysql.createConnection({
-        host: "db",
+        host: mySqlHost,
         port: 3306,
         user: "root",
         password: "admin123",
@@ -147,14 +156,9 @@ function connectToMySQL() {
 // serve up production assets
 app.use(cors())
 app.use(express.json());
-// app.use(express.static(path.resolve(__dirname, '../', 'Client', 'build')));
-// app.use(express.static(path.resolve(__dirname, 'Client', 'build')));
-
 
 app.get('/', (req, res) => {
-    // res.sendFile(path.resolve(__dirname, '../', 'Client', 'build', 'index.html'));
-    // res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'));
-    // res.send('hello from container')
+    res.send('Ping from server')
 });
 
 app.get('/readAll', (req, res) => {
